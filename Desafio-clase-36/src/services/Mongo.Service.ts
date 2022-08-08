f80911320b541cf833cbd5df8e7d4f68934c1e57
Fabricio  Mores <63478471+FMores@users.noreply.github.com>
2022-08-08 19:31:45 -0300

    If para controlar sesiones activas de mongo

--- Desafio-clase-36/src/services/Mongo.Service.ts
+++ Desafio-clase-36/src/services/Mongo.Service.ts
@@ -1,16 +1,20 @@
 import { PersistenceType } from '../DAO/interfaces';
 import config from '../config/index';
-import mongoose from 'mongoose';
+import mongoose, { connection, connect } from 'mongoose';
 import { logger } from '../utils/winston.logger';
 
-export const mongoConnection = async (type: PersistenceType) => {
+export const mongoConnection = async (type: PersistenceType): Promise<typeof mongoose | undefined> => {
 	try {
+		if (connection.readyState === 2) {
+			return;
+		}
+
 		if (type === 'Mongo') {
-			const mongoDbLocal = await mongoose.connect(config.MONGO_LOCAL_URI);
+			const mongoDbLocal = await connect(config.MONGO_LOCAL_URI);
 			logger.info('Successful connection to local mongo database');
 			return mongoDbLocal;
 		} else {
-			const mongoAtlas = await mongoose.connect(config.MONGO_ATLAS_URI);
+			const mongoAtlas = await connect(config.MONGO_ATLAS_URI);
 			logger.info('Successful connection to mongo atlas database');
 
 			return mongoAtlas;
